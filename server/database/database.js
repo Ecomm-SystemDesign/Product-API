39bed69635a4823dcdc609f102c328f88fce6c3b
Jerry Renn <jerryrenn1998@gmail.com>
2023-05-30 12:55:37 -0700

    reconfigure pool .env

--- server/database/database.js
+++ server/database/database.js
@@ -2,11 +2,11 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'products',
-  password: process.env.PASSWORD || 'testing123',
-  port: process.env.PORT || 5432
+  user: process.env.USERNAME,
+  host: process.env.HOST,
+  database: process.env.DATABASE,
+  password: process.env.PASSWORD,
+  port: process.env.DBPORT
 });
 
 pool.connect()
