cf2f86fb4c45fd09a26aa84625ca02d3330d29b3
Jerry Renn <jerryrenn1998@gmail.com>
2023-05-30 10:44:32 -0700

    configure .env

--- server/database/database.js
+++ server/database/database.js
@@ -5,8 +5,8 @@ const pool = new Pool({
   user: 'postgres',
   host: 'localhost',
   database: 'products',
-  password: process.env.password || '',
-  port: process.env.port || 5432
+  password: process.env.PASSWORD || 'testing123',
+  port: process.env.PORT || 5432
 });
 
 pool.connect()
